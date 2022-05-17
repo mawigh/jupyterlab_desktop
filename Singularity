3fd0beeead499048967e7353c9a50fd9b8313db2
Marcel-Brian Wilkowsky <marcel.wilkowsky@hotmail>
2022-05-17 20:04:49 +0200

    create jupyter server proxy using py entry points

--- Singularity
+++ Singularity
@@ -6,7 +6,6 @@ From: debian:bullseye
     apt -y update
     export DEBIAN_FRONTEND=noninteractive
     apt install -y python3 python3-pip procps websockify xterm gcc dbus-x11 xfce4 xfce4-panel xfce4-session xfce4-settings xfce4-terminal xorg wget git vim-tiny vim-gtk3 zsh                                                      
-
     wget https://sourceforge.net/projects/turbovnc/files/2.2.5/turbovnc_2.2.5_amd64.deb/download -O turbovnc_2.2.5_amd64.deb                                                                                      
     apt install -y -q ./turbovnc_2.2.5_amd64.deb
     rm ./turbovnc_2.2.5_amd64.deb
--- src/jupyterlab_desktop/__init__.py
+++ src/jupyterlab_desktop/__init__.py
@@ -1,1 +1,70 @@
-from jupyterlab_desktop import JupyterLabDesktop
+import os;
+import subprocess;
+
+def initVNCServer ():
+
+    instance = JupyterLabDesktop();
+    command = instance.getCommand('{port}');
+
+    desktop_conf = {
+        'command': command,
+        'timeout' : 30,
+        'mappath': {'/': '/vnc_lite.html'},
+        'new_browser_tab': True,
+        'environment': {
+        },
+        'launcher_entry' :  {
+            'enabled': True,
+            'title': 'Remote Desktop (VNC)',
+            'icon_path': JupyterLabDesktop.getnoVNCLogo()
+        }
+    }
+
+    environment = instance.getEnvironment();
+    desktop_conf['environment'].update(environment);
+
+    return desktop_conf;
+
+class JupyterLabDesktop:
+
+    def __init__ (self, loadsingularity=None):
+
+            self.loadsingularity = loadsingularity;
+            
+            self.files = os.path.abspath(os.path.join(os.path.dirname(__file__), "../files"));
+            self.vncStartup = self.files + '/runVNCServer.sh';
+            self.xstartup  = self.files + '/xstartup';
+            self.noVNC = self.files + '/noVNC/';
+
+            self.singularity_cmd = 'singularity';
+            if not self.loadsingularity == None:
+                self.singularity_cmd = self.loadSingularity();
+
+    def getEnvironment (self):
+
+        env = {
+            "SINGULARITY_BIND": self.vncStartup + ':/runVNCServer.sh,' + self.xstartup + ':/xstartup,' + self.noVNC + ':/noVNC',
+        };
+
+        return env;
+
+    def getCommand (self, vncPort=5001):
+
+        singularity_container = self.files + '/jh_desktop.sif';
+
+        full_cmd = self.singularity_cmd + ' exec ' + singularity_container + ' /runVNCServer.sh ' + str(vncPort);
+
+        return full_cmd.split();
+
+    def loadSingularity (self):
+
+        #import lmod;
+        from shutil import which;
+        #lmod.load(self.loadsingularity);
+        singularity_exec = which('singularity');
+        return singularity_exec or None;
+
+    @staticmethod
+    def getnoVNCLogo ():
+        logo = os.path.abspath(os.path.join(os.path.dirname(__file__), "../files/noVNC.svg"));
+        return logo;
